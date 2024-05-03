f9b29c76822b1754b8f6058917829b8df93c9ced
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-04 01:04:07 +1000

    added tree class

---
+++ src/main/java/Tanks/Tree.java
@@ -0,0 +1,52 @@
+package Tanks;
+
+import org.checkerframework.checker.units.qual.A;
+import org.json.JSONTokener;
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
+import java.io.*;
+import java.util.*;
+
+public class Tree {
+
+    private App app;
+    private Level level;
+    private float[] movingAverages;
+    private float xCoordinate;
+    private float yCoordinate;
+    private int size;
+    private PImage treeImage;
+
+    public Tree(App app, Level level, float xCoordinate, float yCoordinate) {
+        this.level = level;
+        this.app = app;
+        this.treeImage = level.getTreeImage();
+        this.size = 32;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public void groundTree() {
+        this.movingAverages = this.level.getBackgroundTerrain().getMovingAveragePoints();
+        this.yCoordinate = this.movingAverages[(int)this.xCoordinate+(this.size/2)]-this.size;
+    }
+
+    public void drawTree() {
+        this.groundTree();
+        app.image(this.treeImage, this.xCoordinate, this.yCoordinate, this.size, this.size);
+    }
+    
+    public void moveTree() {
+        ;
+    }
+}
