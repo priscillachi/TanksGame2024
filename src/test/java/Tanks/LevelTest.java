f2c85b97b075123aa0906af4594a8684232fc200
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-16 04:26:31 +1000

    changes

--- src/test/java/Tanks/LevelTest.java
+++ src/test/java/Tanks/LevelTest.java
@@ -6,5 +6,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LevelTest {
-    
+    App app = new App();
+
+    @Test 
+    public void checkLevelExists() {
+        Level level = new Level(app, 1);
+        assertTrue(level != null);
+    }
 }
