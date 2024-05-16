edf873345403f6f181a543cabd3d8978a3514299
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-17 01:19:04 +1000

    altered JUnit testing methods

--- src/test/java/Tanks/TreeTest.java
+++ src/test/java/Tanks/TreeTest.java
@@ -2,9 +2,27 @@ package Tanks;
 
 
 import processing.core.PApplet;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TreeTest {
+    App app = new App();
+    Tree tree = new Tree(app, app.level1, -1, 30);
+
+    @BeforeEach
+    public void beforeEach() {
+        PApplet.runSketch(new String[] { "App" }, app);
+        app.delay(1000); // to give time to initialise stuff before drawing begins
+    }
+
+    @Test 
+    public void testGroundTree() {
+        assertTrue(tree.groundTree() == true);
+    }
+
+    @Test 
+    public void testDrawTree() {
+        assertTrue(tree.drawTree() == false);
+    }
     
 }
