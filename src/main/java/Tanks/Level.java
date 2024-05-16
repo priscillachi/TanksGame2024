f7e850749604b8ef9dc789ce3eb54544eee3d2ca
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-16 15:41:03 +1000

    changed void methods to boolean for easier testing

--- src/main/java/Tanks/Level.java
+++ src/main/java/Tanks/Level.java
@@ -2,6 +2,9 @@ package Tanks;
 
 import org.checkerframework.checker.units.qual.A;
 import org.json.JSONTokener;
+
+import com.jogamp.common.ExceptionUtils;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PShape;
@@ -161,8 +164,10 @@ public class Level {
 
     /**
      * Initialise trees according to the matrix read from the corresponding level txt file. Call this in the setup() function of the App class.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void setTrees() {
+    public boolean setTrees() {
 
         if (!app.levelsData.getJSONObject(level-1).isNull("trees")) { // make sure "trees" field is available
             setTerrainMatrix(); // get matrix
@@ -184,14 +189,20 @@ public class Level {
                 Tree treeObj = new Tree(app, this, this.treeCoordinates.get(i)[0], this.treeCoordinates.get(i)[1]);
                 this.trees.add(treeObj);
             }
+
+            return true;
         }
+
+        return false;
     }
 
 
     /**
      * Initialise players according to the matrix read from the corresponding level txt file. Call this in the setup() function of the App class.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void setPlayers() { // setup players coordinates, objects, tanks,etc.
+    public boolean setPlayers() { // setup players coordinates, objects, tanks,etc.
         setTerrainMatrix();
 
         for (int i=0; i<28; i++) {
@@ -229,6 +240,12 @@ public class Level {
             this.playersObj.add(playerObj);
             playerObj.setTank();
             playerObj.setHealthPower();
+        }
+
+        if (this.playersCoordinates.size() > 0 && this.colourSchemes.size() > 0 && this.playersObj.size() > 0) {
+            return true;
+        } else {
+            return false;
         }
 
     }
@@ -304,107 +321,155 @@ public class Level {
 
     /**
      * Display the scoreboard at the top right corner of the game.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void displayScoreboard() { // what the method says
-        app.strokeWeight(5);
-        app.stroke(0);
-        app.noFill();
-        app.rect(700, 50, 150, 28*(1+this.players.size()));
-        app.line(700, 50+28, 850, 50+28);
-        app.strokeWeight(1);
-        app.textSize(18);
-        app.fill(0);
-        app.text("Scores", 710, 50+20);
-        for (int i=0; i<this.playersObj.size(); i++) {
-            String playerPrint = String.format("Player %s", this.playersObj.get(i).getPlayerString());
-            String playerScore = String.format("%d", this.playersObj.get(i).getScore());
-            int[] colourScheme = this.colourSchemes.get(i);
-            app.fill(colourScheme[0], colourScheme[1], colourScheme[2]);
-            app.text(playerPrint, 710, 50+(26*(i+2)));
+    public boolean displayScoreboard() { // what the method says
 
+        try {
+            app.strokeWeight(5);
+            app.stroke(0);
+            app.noFill();
+            app.rect(700, 50, 150, 28*(1+this.players.size()));
+            app.line(700, 50+28, 850, 50+28);
+            app.strokeWeight(1);
+            app.textSize(18);
             app.fill(0);
-            app.text(playerScore, 810, 50+(26*(i+2)));
+            app.text("Scores", 710, 50+20);
+            for (int i=0; i<this.playersObj.size(); i++) {
+                String playerPrint = String.format("Player %s", this.playersObj.get(i).getPlayerString());
+                String playerScore = String.format("%d", this.playersObj.get(i).getScore());
+                int[] colourScheme = this.colourSchemes.get(i);
+                app.fill(colourScheme[0], colourScheme[1], colourScheme[2]);
+                app.text(playerPrint, 710, 50+(26*(i+2)));
+    
+                app.fill(0);
+                app.text(playerScore, 810, 50+(26*(i+2)));
+            }
+
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
     /**
      * Display wind at the top right corner of the game.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void displayWind() { // what the method says
-        this.windPositiveImage = app.loadImage(app.getClass().getResource("wind.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " ")); // get image
-        this.windNegativeImage = app.loadImage(app.getClass().getResource("wind-1.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " ")); // get image
+    public boolean displayWind() { // what the method says
+        try {
+            this.windPositiveImage = app.loadImage(app.getClass().getResource("wind.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " ")); // get image
+            this.windNegativeImage = app.loadImage(app.getClass().getResource("wind-1.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " ")); // get image
+    
+            if (this.windLevel >= 0) {
+                app.image(this.windPositiveImage, 750, 2, 50, 50);
+                String wind = String.format("%d", this.windLevel);
+                app.strokeWeight(1);
+                app.textSize(18);
+                app.fill(0);
+                app.text(wind, 810, 30);
+            } else if (this.windLevel < 0) {
+                app.image(this.windNegativeImage, 750, 2, 50, 50);
+                String wind = String.format("%d", Math.abs(this.windLevel));
+                app.strokeWeight(1);
+                app.textSize(18);
+                app.fill(0);
+                app.text(wind, 810, 30);
+            }
 
-        if (this.windLevel >= 0) {
-            app.image(this.windPositiveImage, 750, 2, 50, 50);
-            String wind = String.format("%d", this.windLevel);
-            app.strokeWeight(1);
-            app.textSize(18);
-            app.fill(0);
-            app.text(wind, 810, 30);
-        } else if (this.windLevel < 0) {
-            app.image(this.windNegativeImage, 750, 2, 50, 50);
-            String wind = String.format("%d", Math.abs(this.windLevel));
-            app.strokeWeight(1);
-            app.textSize(18);
-            app.fill(0);
-            app.text(wind, 810, 30);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
+
     }
 
     /**
-     * Display fuel and parachute images at the top right corner of the game.
+     * Display fuel and parachute images at the top left corner of the game.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void displayFuelParachute() { // what the method says
-        fuelImage = app.loadImage(app.getClass().getResource("fuel.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " "));
-        app.image(fuelImage, 180, 5, 28, 28);
-        this.turn.displayFuel();
+    public boolean displayFuelParachute() { // what the method says
+        try {
+            fuelImage = app.loadImage(app.getClass().getResource("fuel.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " "));
+            app.image(fuelImage, 180, 5, 28, 28);
+            this.turn.displayFuel();
+    
+            parachuteImage = app.loadImage(app.getClass().getResource("parachute.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " "));
+            app.image(parachuteImage, 180, 35, 28, 28);
+            this.turn.displayParachute();
 
-        parachuteImage = app.loadImage(app.getClass().getResource("parachute.png").getPath().toLowerCase(Locale.ROOT).replace("%20", " "));
-        app.image(parachuteImage, 180, 35, 28, 28);
-        this.turn.displayParachute();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
-    public void displayShield() {
-        int[] shieldColour = this.turn.getColourScheme();
-        app.fill(shieldColour[0], shieldColour[1], shieldColour[2], 50);
-        app.ellipse(194,78,26,26);
-        this.turn.displayShield();
+    /**
+     * Display shield symbol at the top left corner of the game.
+     * 
+     * @return true if executed, false if otherwise.
+     */
+    public boolean displayShield() {
+        try {
+            int[] shieldColour = this.turn.getColourScheme();
+            app.fill(shieldColour[0], shieldColour[1], shieldColour[2], 50);
+            app.ellipse(194,78,26,26);
+            this.turn.displayShield();
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
     /**
      * Display final scoreboard in descending score order. Call at the end of the game.
+     * 
+     * @return true if executed, false if otherwise.
      */
-    public void displayFinalScoreboard() {
-        this.sortScores(); // sort descending
-        app.textSize(22);
-        app.strokeWeight(1);
-        app.fill(0);
-        String winnerPlayer = String.format("Player %s wins!", this.playersObj.get(0).getPlayerString());
-        app.text(winnerPlayer, 288, 203);
-        int[] winnerColour = this.colourSchemes.get(0);
-        app.fill(winnerColour[0], winnerColour[1], winnerColour[2], 25);
-        app.strokeWeight(5);
-        app.stroke(0);
-        app.rect(288, 240, 170, 30*(2+this.playersObj.size()));
-        app.line(288, 280, 458, 280);
-        app.fill(0);
-        app.text("Final Scores", 298, 270);
-
-        for (int i=0; i<this.playersObj.size(); i++) {
-
-            String playerPrint = String.format("Player %s", this.playersObj.get(i).getPlayerString());
-            String playerScore = String.format("%d", this.playersObj.get(i).getScore());
-            int[] colourScheme = this.colourSchemes.get(i);
-
-            if (this.finalScoreboardCount >= 21 * i) { // 0.7s buffer between each player and score displayed
-                app.fill(colourScheme[0], colourScheme[1], colourScheme[2]);
-                app.text(playerPrint, 298, 250+(30*(i+2)));
+    public boolean displayFinalScoreboard() {
+        try {
+            this.sortScores(); // sort descending
+            app.textSize(22);
+            app.strokeWeight(1);
+            app.fill(0);
+            String winnerPlayer = String.format("Player %s wins!", this.playersObj.get(0).getPlayerString());
+            app.text(winnerPlayer, 288, 203);
+            int[] winnerColour = this.colourSchemes.get(0);
+            app.fill(winnerColour[0], winnerColour[1], winnerColour[2], 25);
+            app.strokeWeight(5);
+            app.stroke(0);
+            app.rect(288, 240, 170, 30*(2+this.playersObj.size()));
+            app.line(288, 280, 458, 280);
+            app.fill(0);
+            app.text("Final Scores", 298, 270);
     
-                app.fill(0);
-                app.text(playerScore, 405, 250+(30*(i+2)));
+            for (int i=0; i<this.playersObj.size(); i++) {
+    
+                String playerPrint = String.format("Player %s", this.playersObj.get(i).getPlayerString());
+                String playerScore = String.format("%d", this.playersObj.get(i).getScore());
+                int[] colourScheme = this.colourSchemes.get(i);
+    
+                if (this.finalScoreboardCount >= 21 * i) { // 0.7s buffer between each player and score displayed
+                    app.fill(colourScheme[0], colourScheme[1], colourScheme[2]);
+                    app.text(playerPrint, 298, 250+(30*(i+2)));
+        
+                    app.fill(0);
+                    app.text(playerScore, 405, 250+(30*(i+2)));
+                }
             }
+    
+            this.finalScoreboardCount += 1; // to make sure there is 0.7s between scores displayed
+
+            return true;
+        } catch (Exception e) {
+            return false;
         }
 
-        this.finalScoreboardCount += 1; // to make sure there is 0.7s between scores displayed
     }
 }
