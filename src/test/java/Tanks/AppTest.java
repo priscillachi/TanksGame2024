13460b3c041892b632cc86137a9e10c0b9dd2b21
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-17 01:17:57 +1000

    altered JUnit testing methods

--- src/test/java/Tanks/AppTest.java
+++ src/test/java/Tanks/AppTest.java
@@ -2,14 +2,38 @@ package Tanks;
 
 
 import processing.core.PApplet;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+    App app = new App();
+
+    @BeforeEach
+    public void beforeEach() {
+        PApplet.runSketch(new String[] { "App" }, app);
+        app.delay(1000); // to give time to initialise stuff before drawing begins
+    }
 
     @Test
     public void checkAppExists() {
-        App app = new App();
         assertTrue(app != null);
+    }
+
+    @Test 
+    public void appTest() {
+        app.draw();
+        assertTrue(app.playersNumber == app.currentLevel.getAlivePlayers().size());
+
+        app.currentLevel.getAlivePlayers().get(0).setTank();
+        app.currentLevel.getAlivePlayers().get(0).getTank().setAngle((float)0.2);
+        app.currentLevel.getAlivePlayers().get(0).getTank().setProjectile();
+        app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().setProjectileShot(true);
+        app.draw();
+        assertTrue(app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().getXCoordinate()!=app.currentLevel.getAlivePlayers().get(0).getTank().getXCoordinate());
+        assertTrue(app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().getYCoordinate()!=app.currentLevel.getAlivePlayers().get(0).getTank().getYCoordinate());
+
+        app.currentLevel.getAlivePlayers().get(0).getHealthPower().updateHealth(0);
+        app.draw();
+        assertTrue(app.currentLevel.getAlivePlayers().get(0).getPlayerAlive()==false);
     }
 }
