7c0ccd5a4b2c58756694b1143ef08c77e6966206
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-17 01:18:20 +1000

    altered JUnit testing methods

--- src/test/java/Tanks/LevelTest.java
+++ src/test/java/Tanks/LevelTest.java
@@ -2,15 +2,28 @@ package Tanks;
 
 
 import processing.core.PApplet;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LevelTest {
     App app = new App();
 
-    @Test 
-    public void checkLevelExists() {
-        Level level = new Level(app, 1);
-        assertTrue(level != null);
+    @BeforeEach
+    public void beforeEach() {
+        PApplet.runSketch(new String[] { "App" }, app);
+        app.delay(1000); // to give time to initialise stuff before drawing begins
     }
+
+    @Test 
+    public void checkLevelsExist() {
+        assertTrue(app.level1 != null);
+        assertTrue(app.level2 != null);
+        assertTrue(app.level3 != null);
+    }
+
+    @Test 
+    public void displayFinalScoreboard() {
+        assertTrue(app.level1.displayFinalScoreboard() == true);
+    }
+
 }
