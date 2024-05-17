5190bb3843019fe4111755da9ffb5c926b755510
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-17 22:59:06 +1000

    final changes before submission

--- src/test/java/Tanks/ProjectileTest.java
+++ src/test/java/Tanks/ProjectileTest.java
@@ -9,62 +9,62 @@ public class ProjectileTest {
     App app = new App();
     int[] colour = {255, 0, 0};
     Player player = new Player(app, app.level1, 20, 20, colour, "X");
-    HealthPower healthPower = new HealthPower(app, app.level2, player);
+    HealthPower healthPower = new HealthPower(app, app.level1, player);
     Tank tank = new Tank(20, 20, colour, app, app.level1, player);
     Projectile projectile = new Projectile(app, tank, 20, 20, tank.getAngle(), healthPower.getPower(), 4, colour);
 
     @BeforeEach
     public void beforeEach() {
-        PApplet.runSketch(new String[] { "App" }, app);
-        app.delay(1000); // to give time to initialise stuff before drawing begins
+        PApplet.runSketch(new String[] { "App" }, this.app);
+        this.app.delay(1000); 
     }
 
     @Test 
     public void updateVelocityCoordinates() {
-        projectile.setWind(-15);
-        projectile.setAngle((float)-0.2);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setWind(-15);
+        this.projectile.setAngle((float)-0.2);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
 
-        projectile.setWind(15);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setWind(15);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
 
-        projectile.setAngle((float)0.35);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setAngle((float)0.35);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
 
-        projectile.setWind(-15);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setWind(-15);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
 
-        projectile.setAngle(0);
-        tank.setAngle(-(float)Math.PI/2);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setAngle(0);
+        this.tank.setAngle(-(float)Math.PI/2);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
 
-        projectile.setAngle(0);
-        tank.setAngle((float)Math.PI/2);
-        projectile.updateVelocityCoordinates();
-        assertTrue(projectile.getXCoordinate()!=0 && projectile.getYCoordinate()!=0);
+        this.projectile.setAngle(0);
+        this.tank.setAngle((float)Math.PI/2);
+        this.projectile.updateVelocityCoordinates();
+        assertTrue(this.projectile.getXCoordinate()!=0 && this.projectile.getYCoordinate()!=0);
     }
 
     @Test 
     public void drawProjectile() {
-        projectile.setProjectileShot(true);
-        assertTrue(projectile.drawProjectile()==true);
+        this.projectile.setProjectileShot(true);
+        assertTrue(this.projectile.drawProjectile()==true); // assert true
 
-        projectile.setProjectileShot(false);
-        assertFalse(projectile.drawProjectile()==true);
+        this.projectile.setProjectileShot(false);
+        assertFalse(this.projectile.drawProjectile()==true); // assert false
     }
 
     @Test 
     public void drawExplosion() {
-        projectile.setExplosionOut(true);
-        assertTrue(projectile.drawExplosion()==true);
+        this.projectile.setExplosionOut(true);
+        assertTrue(this.projectile.drawExplosion()==true); // assert true
 
-        projectile.setExplosionOut(false);
-        assertFalse(projectile.drawExplosion()==true);
+        this.projectile.setExplosionOut(false);
+        assertFalse(this.projectile.drawExplosion()==true); // assert false
     }
     
 }
