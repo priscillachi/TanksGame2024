50394f93422665a9b8616d0ee52de327a603a7e6
Priscilla Nguyen <chi._nguyen@outlook.com>
2024-05-17 15:06:38 +1000

    added methods

--- src/test/java/Tanks/AppTest.java
+++ src/test/java/Tanks/AppTest.java
@@ -36,4 +36,53 @@ public class AppTest {
         app.draw();
         assertTrue(app.currentLevel.getAlivePlayers().get(0).getPlayerAlive()==false);
     }
+
+    @Test
+    public void parachuteTest() {
+        app.currentLevel.getAlivePlayers().get(0).setTank();
+        app.currentLevel.getAlivePlayers().get(0).getTank().setAngle((float)1.57);
+        app.currentLevel.getAlivePlayers().get(0).getTank().setProjectile();
+        app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().setProjectileShot(true);
+        app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().setXCoordinate(20);
+        app.currentLevel.getAlivePlayers().get(0).setParachuteOn(false);
+        for (int i=0; i<30; i++) {
+            app.draw();
+            i+=1;
+        }
+        assertTrue(app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().getProjectileShot()==true);
+
+        app.currentLevel.getAlivePlayers().get(0).setParachuteOn(true);
+        app.currentLevel.getAlivePlayers().get(0).getTank().setYCoordinate(app.currentLevel.getAlivePlayers().get(0).getTank().getYCoordinate()-10);
+        for (int i=0; i<30; i++) {
+            app.draw();
+            i += 1;
+        }
+        assertTrue(app.currentLevel.getAlivePlayers().get(0).getHealthPower().getLoseHealth()==false);
+    }
+
+
+    @Test 
+    public void belowMap() {
+        app.currentLevel.getAlivePlayers().get(0).setTank();
+        app.currentLevel.getAlivePlayers().get(0).getTank().setAngle((float)1.57);
+        app.currentLevel.getAlivePlayers().get(0).getTank().setProjectile();
+        app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().setProjectileShot(true);
+        app.currentLevel.getAlivePlayers().get(0).getTank().getProjectile().setXCoordinate(20);
+        app.currentLevel.getAlivePlayers().get(0).setParachuteOn(false);
+        for (int i=(int)app.currentLevel.getAlivePlayers().get(0).getTank().getXCoordinate()-30; i<(int)app.currentLevel.getAlivePlayers().get(0).getTank().getXCoordinate()+60; i++) {
+            app.currentLevel.getBackgroundTerrain().updateTerrain(i, 750);
+            i+=1;
+        }
+
+        app.currentLevel.getAlivePlayers().get(0).getTank().setYCoordinate(700);
+        app.currentLevel.getAlivePlayers().get(0).setParachuteOn(true);
+        for (int i=0; i<30; i++) {
+            app.draw();
+            i += 1;
+        }
+
+        app.currentLevel.getAlivePlayers().get(0).getTank().setExplosionRadius(30);
+        app.draw();
+        //assertTrue(app.currentLevel.getAlivePlayers().size()!=0);
+    }
 }
